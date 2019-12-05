dfe0e5e1d46b63bf7b764471c7ccc393fd28d6bf
SARademakers <2049137@talnet.nl>
2019-12-05 14:10:06 +0100

    Uitwerking van de exercise

---
+++ galgje.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<?php
+session_start();
+$woorden = array("hangman", "computer", "variable", "integer");
+?>
+    <head>
+        <title>Galgje</title>
+        <link href="style.css" rel="Stylesheet">
+    </head>
--- index.php
+++ index.php
@@ -1,24 +1,30 @@
 <!DOCTYPE html>
 <html>
-<?php
-$woorden = array("hangman", "computer", "variable", "integer");
-?>
     <head>
         <title>Galgje</title>
+        <link href="style.css" rel="Stylesheet"> 
     </head>
     <body>
+        <center>
         <h1> Galgje </h1>
         <form action="" method='POST'>
         <input type='submit' id='btn' name='keuze' value='Kies zelf een woord'>
-        <input type='submit' id='btn' name='random' value='Kies een willekeurig woord'>
+        <input type='submit' id='btn' name='random' value='Kies een willekeurig woord' href='galgje.php'>
         </form>
-
 <?php
 if (isset($_POST["keuze"])) {
-    echo "Kies je woord: " . PHP_EOL;
-    echo '<input type="text" name="woord">' . PHP_EOL;
-    echo '<input type="submit" name="submit" value="Submit"> ';
+    echo '<form action="galgje.php" method = "POST">';
+    echo "Je hebt gekozen om zelf een woord in te voeren: <br>";
+    echo '<input type="text" name="woord">';
+    echo '<input type="submit" name="submit" value="Speel met dit woord"> ';
+    echo '</form>';
+}
+if (isset($_POST["random"])) {
+    echo '<form action="galgje.php" method = "POST">';
+    echo "Je hebt gekozen om met een willekeurig woord te spelen. <br>";
+    echo '<input type="submit" name="submit" value="Speel met een willekeurig woord"> ';
 }
 ?>
+        </center>
     </body>
 </html>
