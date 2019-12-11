a823e43f4bcc9941d15c25ab6d7be27b41e0eb49
SARademakers <2049137@talnet.nl>
2019-12-11 10:22:30 +0100

    Uitwerking van de exercise

--- galgje.php
+++ galgje.php
@@ -1,10 +1,62 @@
 <!DOCTYPE html>
 <html>
 <?php
+
 session_start();
+
+setcookie ("levens", 4);
 $woorden = array("hangman", "computer", "variable", "integer");
+echo  $_POST["woord"] . "<br>";
 ?>
     <head>
         <title>Galgje</title>
         <link href="style.css" rel="Stylesheet">
     </head>
+    <body>
+
+<?php
+echo ($_COOKIE['random']);
+if (isset($_POST["letter"]))   {
+    $_COOKIE["levens"] -= 1;
+    setcookie ("levens", $_COOKIE["levens"]);
+    echo "<h1>Je hebt: " . $_COOKIE["levens"] . " levens </h1>";
+}
+
+switch ($_COOKIE["levens"]) {
+    case "4";
+    echo "<img class='h1'
+    src='img/h1.png'
+    alt='h1'>";
+break;
+    case "3";
+    echo "<img class='h2'
+    src='img/h2.png'
+    alt='h2'>";
+break;
+    case "2";
+    echo "<img class='h3'
+    src='img/h3.png'            
+    alt='h3'>";
+break;
+    case "1";
+    echo "<img class='h4'
+    src='img/h4.png'
+    alt='h4'>";
+break;
+    case "0";
+    echo "<img class='h5'
+    src='img/h5.png'
+    alt='h5'>";
+    $_COOKIE["levens"] = 5;
+    setcookie ("levens", $_COOKIE["levens"]);
+    echo "Klik op submit om opnieuw te proberen of ga terug naar de homepage voor een ander woord.";
+}
+?>
+        <form action="" method='POST'>
+            <label for = "letter">Vul hier je letter in: </label> 
+            <input type="text" id="letter" name="letter" maxlength="1">
+            <input type="submit" id="antwoord"`name="antwoord" value="submit ">
+        </form>
+
+    </body>
+</html>
--- index.php
+++ index.php
@@ -9,9 +9,9 @@
         <h1> Galgje </h1>
         <form action="" method='POST'>
         <input type='submit' id='btn' name='keuze' value='Kies zelf een woord'>
-        <input type='submit' id='btn' name='random' value='Kies een willekeurig woord' href='galgje.php'>
+        <input type='submit' id='btn' name='random' value='Kies een willekeurig woord'>
         </form>
-<?php
+<?php   
 if (isset($_POST["keuze"])) {
     echo '<form action="galgje.php" method = "POST">';
     echo "Je hebt gekozen om zelf een woord in te voeren: <br>";
@@ -23,6 +23,10 @@ if (isset($_POST["random"])) {
     echo '<form action="galgje.php" method = "POST">';
     echo "Je hebt gekozen om met een willekeurig woord te spelen. <br>";
     echo '<input type="submit" name="submit" value="Speel met een willekeurig woord"> ';
+    $woorden = array("hangman", "computer", "variable", "integer");
+    $randIndex = array_rand($woorden);
+    $random = $woorden[$randIndex];
+    setcookie('random', $random);
 }
 ?>
         </center>
